358e03588f274d2b7c8b1b9bb41e2b6e844184a6
tristokrepmita <tristan.vujovic@gmail.com>
2020-02-12 15:43:51 +0100

    Added multiple user replying, fixed default image not showing, added comments and documentation, updated requirements

--- cogs/reply.py
+++ cogs/reply.py
@@ -1,9 +1,10 @@
 from discord.ext.commands import Cog, command
-from discord import Embed, File
+from discord.utils import get
+from discord.errors import InvalidArgument
+from discord import Webhook, AsyncWebhookAdapter, Embed, File
 import aiohttp
-from discord import Webhook, AsyncWebhookAdapter
-from urllib.request import Request, urlopen
 import os
+
 
 class Reply(Cog):
     def __init__(self, bot):
@@ -15,54 +16,78 @@ class Reply(Cog):
 
     @Cog.listener()
     async def on_raw_reaction_add(self, payload):
+        """
+        When a reaction is added to any message the user and that message are linked
+        within a message_to_user dictionary
+        Users can only reply to one message at a time therefore if a user
+        reacts to a new message when their id is already within message_to_user dictionary,
+        the old message id gets overwritten by the new one
+
+        Multiple users can react to the same message
+        """
         with open("emoji", "r", encoding="utf-8") as f:
             self.emoji = f.read()
         #   If user has a message queued up to be replied to it will be overwritten
-        if payload.user_id in self.message_to_user.values():
-            del self.message_to_user[dict(zip(self.message_to_user.values(), self.message_to_user.keys()))[payload.user_id]]
+        if payload.user_id in self.message_to_user.keys():
+            del self.message_to_user[payload.user_id]
         if payload.emoji.name == self.emoji:
             channel = self.bot.get_channel(payload.channel_id)
             message = await channel.fetch_message(payload.message_id)
             user = self.bot.get_user(payload.user_id)
-            self.message_to_user[message.id] = user.id
+            self.message_to_user[user.id] = message.id
 
     @Cog.listener()
     async def on_raw_reaction_remove(self, payload):
+        """
+        If a user removes their reaction from a message his id
+        is removed from the message_to_user dict which removes
+        him from the reply queue
+
+        This also gets called when the bot itself removes reactions
+        but is ignored
+        """
+        #   payload.user_id actually returns the user whose emoji got removed
+        #   instead of returning the id of the user who removed it
         with open("emoji", "r", encoding="utf-8") as f:
             self.emoji = f.read()
         if payload.emoji.name == self.emoji:
-            print("User removed from replying")
-            channel = self.bot.get_channel(payload.channel_id)
-            msg = await channel.fetch_message(payload.message_id)
-            del self.message_to_user[msg.id]
+            if self.message_to_user.get(payload.user_id):
+                del self.message_to_user[payload.user_id]
 
     @Cog.listener()
     async def on_message(self, msg):
-        if msg.author.id in list(self.message_to_user.values()):
-            if msg.attachments:
-                req = Request(url=msg.attachments[0].url, headers={'User-Agent': 'Mozilla/5.0'})
-                webpage = urlopen(req).read()
-                with open(msg.attachments[0].filename, 'wb') as f:
-                    f.write(webpage)
-
-            message = await msg.channel.fetch_message(dict(zip(self.message_to_user.values(), self.message_to_user.keys()))[msg.author.id])
-            del self.message_to_user[message.id]
+        """
+        When a message is sent check if that message is actually a reply
+        to another message and if so delete it and turn it into a webhooked reply
+        """
+        if msg.author.id in list(self.message_to_user.keys()):
+            message = await msg.channel.fetch_message(self.message_to_user[msg.author.id])
+            del self.message_to_user[msg.author.id]
             webhook = await msg.channel.create_webhook(name="Placeholder")
             await self.send_message(msg.author, await self.create_embed(message.author, message), msg, webhook)
             await webhook.delete()
-            await message.clear_reactions()
+            emoji = get(msg.channel.guild.emojis, name=self.emoji)
+            try:
+                await message.remove_reaction(emoji, msg.author)
+            except InvalidArgument:
+                await message.remove_reaction(self.emoji, msg.author)
             await msg.delete()
 
     async def create_embed(self, author, author_message):
+        """
+        Create the embed with the contents of the message that is being
+        replied to
+        :param author: Author (User object) of the message
+        :param author_message: The contents of the message
+        :return: Embed object with the message contents and user info along with
+        hyperlink to the message being replied to
+        """
         embed = Embed(color=author.color)
 
         if author_message.clean_content:
             embed.add_field(name=author.display_name, value=author_message.clean_content)
         else:
             embed.add_field(name=author.display_name, value="\u200b")
-
-        # if author_message.attachments:
-        #     print(author_message.attachments[0])
 
         if author_message.attachments:
             for att in author_message.attachments:
@@ -86,13 +111,21 @@ class Reply(Cog):
                 embed.set_image(url="")
                 embed.add_field(name=author.display_name, value=author_message.clean_content)
 
-        embed.set_thumbnail(url=f"https://cdn.discordapp.com/avatars/{author.id}/{author.avatar}.png?size=128")
+        embed.set_thumbnail(url=author.avatar_url_as(size=128, format='png'))
         embed.add_field(name="\u200b", value=f"[⏫⏫⏫⏫]({author_message.jump_url})", inline=False)
 
         return embed
 
     async def send_message(self, original_user, embed, message, webhook):
-        avatar_url = f"https://cdn.discordapp.com/avatars/{original_user.id}/{original_user.avatar}.png?size=128"
+        """
+        Sends a message impersonating the user that called the reply
+        functionality
+        :param original_user: User who send the reply message
+        :param embed: Embed object that contains original_user info and message content
+        :param message: Message of the reply that original_user sent
+        :param webhook: Webhook that will get customized to impersonate original_user
+        """
+        avatar_url = original_user.avatar_url_as(size=128, format='png')
         async with aiohttp.ClientSession() as session:
             webhook = Webhook.from_url(webhook.url, adapter=AsyncWebhookAdapter(session))
 
@@ -141,6 +174,12 @@ class Reply(Cog):
 
     @command(aliases=['change', 'change_emoji', 'replace_emoji', 'emoji_change', 'emoji_replace'])
     async def replace(self, ctx, emoji):
+        """
+        Used to replace the emoji you want to use as a reply button
+        :param ctx: Context of the command to get from which channel it was called
+        :param emoji: The string of the emoji (if it's a custom one strip it of the id part
+        :return: Writes the emoji in the emoji file
+        """
         await ctx.message.channel.send(f"Reply emoji changed to {emoji}")
         if '<' in emoji:
             emoji = emoji.split(':')[1]
--- reply_bot.py
+++ reply_bot.py
@@ -1,4 +1,4 @@
-from discord.ext.commands import Bot, HelpCommand
+from discord.ext.commands import Bot
 from discord import Embed
 from cogs.reply import Reply
 
@@ -9,11 +9,11 @@ client.add_cog(Reply(client))
 
 @client.command()
 async def help(ctx):
-    embed = Embed(color=0x990000)
+    embed = Embed(color=0x45884)
     embed.add_field(name="The Reply Bot", value="\u200b")
     embed.set_thumbnail(url="https://cdn4.iconfinder.com/data/icons/email-2-2/32/Reply-all-Email-512.png")
     embed.add_field(name="Description:", value="A bot that adds reply functionality to Discord")
     embed.add_field(name="Usage:", value="To reply to a message simply react to a message with ✉ and write your response")
     await ctx.message.channel.send(embed=embed)
 
-client.run('BOT_SECRET')
+client.run('Njc3MTMxMDU0NDg1NjAyMzIz.XkPxdA.8w5MJxkzKaoNkNrypZYppLXQ0MI')
